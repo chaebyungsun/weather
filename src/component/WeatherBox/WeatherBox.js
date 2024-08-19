47aecd43658d0423f5fc3ec7f6e58d8bcd30cf73
chaebyungsun <rhemdgkrtod3@naver.com>
2024-08-19 20:03:52 +0900

    재수정

--- src/component/WeatherBox/WeatherBox.js
+++ src/component/WeatherBox/WeatherBox.js
@@ -6,9 +6,10 @@ const WeatherBox = ({ weather }) => {
     <div className="Weather_box">
       <div>{weather?.name}</div>
       <h2>
-        {weather?.main.temp}C / {(weather?.main.temp * 1.8 + 32).toFixed(2)}F
+        {weather?.main?.temp ?? 0}C /{" "}
+        {((weather?.main?.temp ?? 0) * 1.8 + 32).toFixed(2)}F
       </h2>
-      <h3>{weather?.weather[0].description}</h3>
+      <h3>{weather?.weather[0]?.description}</h3>
     </div>
   );
 };
